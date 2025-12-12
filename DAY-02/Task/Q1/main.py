dc587cb69133acb2d0346417390b9c56e031373a
dipratna kamble <dipratnakamble1@gmail.com>
2025-12-12 12:44:32 +0530

    day_2_task

---
+++ DAY-02/Task/Q1/main.py
@@ -0,0 +1,7 @@
+import math_util as mu
+
+print("Area of circle:", mu.area_circle(5))
+print("Area of rectangle:", mu.area_rectangle(10, 4))
+print("Area of square:", mu.area_square(6))
+print("Area of triangle:", mu.area_triangle(8, 3))
+
---
+++ DAY-02/Task/Q4/data.py
@@ -0,0 +1,19 @@
+import requests
+city = input("Enter city name: ")
+import os
+api_key = os.getenv("API_KEY")
+try:
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    print("status code:", response.status_code)
+    # print("response text:", response.text)
+    data = response.json()
+    print("resp data: ", data)
+
+    with open("weather_report.txt", "w") as file:
+        file.write(f"City: {data['name']}\n")
+        file.write(f"Temperature: {data['main']['temp']} °C\n")
+        file.write(f"Humidity: {data['main']['humidity']} %\n")
+        file.write(f"Wind Speed: {data['wind']['speed']} m/s\n")
+except:
+    print("Some error occured.")
